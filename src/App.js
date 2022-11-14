f1de36e8a2133bae8ca0523f9eb70b7e0c4b9191
Huw Carpenter <huwcarpenter@gmail.com>
2022-11-14 12:17:57 +0000

    Add slider on homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from 'react';
 
 import Header from './components/Layout/Header';
+import Slider from './components/Layout/Slider';
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <Slider/>
     </Fragment>
   );
 }
---
+++ src/components/Layout/Slider.js
@@ -0,0 +1,11 @@
+import classes from './Slider.module.css';
+
+const Slider = (props) => {
+  return (
+    <h1 className={classes.slider}>
+      Want to level up your<span></span>
+    </h1>
+  );
+};
+
+export default Slider;
